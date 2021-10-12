864a3ff52d35fea21667db6888cf6e1acbfb1a7f
Mayra Borda <mayra.borda.4@gmail.com>
2021-10-12 17:58:42 -0300

    código optimizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CartContextProvider } from "../src/context/CartContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ItemListContainer from "../src/components/containers/ItemListContainer";
+import ItemDetailContainer from "../src/components/containers/ItemDetailContainer";
+import Cart from "./components/Cart/Cart";
+import './App.css'
+import NavBar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartContextProvider>
+      <Router>
+        <NavBar />
+          <div className="block z-0 h-full bg-gray-100">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/category/:category">
+                <ItemListContainer />
+              </Route>
+              <Route exact path="/details/:id">
+                <ItemDetailContainer />
+              </Route>
+              <Route exact path="/cart">
+                <Cart />
+              </Route>
+            </Switch>
+          </div>
+        <Footer />
+      </Router>{""}
+    </CartContextProvider>
   );
-}
+};
 
 export default App;
